c8d27c13847b10e1346f471a315067e20ec8fd65
Deeksha608 <saideeksha608@gmail.com>
2024-12-28 16:25:54 +0530

    Added calculation

--- src/test/java/GITdemo/Gitdemo/AppTest.java
+++ src/test/java/GITdemo/Gitdemo/AppTest.java
@@ -1,11 +1,15 @@
 package GITdemo.Gitdemo;
 
 public class AppTest {
+	
+	int a =10;
+	int r=9;
+	int c=a+r;
     
     public void main() {
+    	
     		System.out.println("Example one");
-        System.out.println("Edit project");
-        
-    }
-    
+    		System.out.println("Edit project");
+    		System.out.println("Edit project" +c);
+    }   
 }
